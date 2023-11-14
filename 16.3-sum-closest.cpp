e2925b2ee48eac87d5c4f4b6c332d61188d12554
popshia <popshia@gmail.com>
2023-11-14 19:06:25 +0800

    add 3-sum-closest.

---
+++ 16.3-sum-closest.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=16 lang=cpp
+ *
+ * [16] 3Sum Closest
+ */
+
+#include <algorithm>
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    int threeSumClosest(vector<int> &nums, int target) {
+        sort(nums.front(), nums.back());
+        return 0;
+    }
+};
+// @lc code=end
